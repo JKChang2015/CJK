df80705651bbc135df675f66868b4778a9df52ce
JKChang <jkchang@ebi.ac.uk>
2016-04-22 14:57:42 +0100

    Hanoi tower

---
+++ CJK/src/cjk/CJK.java
@@ -0,0 +1,18 @@
+package cjk;
+
+import cjk.hanoi;
+
+public class CJK {
+
+
+
+    public static void main(String[] args) {
+        
+        hanoi ha = new hanoi();
+
+        ha.hanoi('a', 'b', 'c', 3);
+
+    }
+
+
+}
