e54e282eb79d5cbb72a8f36825f3d3795af9c399
JKChang2015 <jkchang2015@gmail.com>
2016-05-17 15:05:16 +0100

    check label

--- CJK/src/cjk/CJK.java
+++ CJK/src/cjk/CJK.java
@@ -3,20 +3,43 @@ package cjk;
 import eNM.IrisChecker;
 import cjk.hanoi;
 import cjk.CombinFile;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.File;
+import java.util.Set;
+import java.util.HashSet;
 import org.semanticweb.owlapi.*;
+
 
 public class CJK {
 
     public static void main(String[] args) {
 
-        String kPath = "";
-        String ontoPath = "";
+        String kPath = "C:\\Users\\jkchang\\Desktop\\keyword.txt";
+        String ontoPath = "C:\\Users\\jkchang\\Desktop\\enanomapper.owl";
+        Set<String> keySet = new HashSet<String>();
+        Set<String> labelSet = new HashSet<String>();
 
+        File file = new File(kPath);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                keySet.add(line.trim());
+                line = reader.readLine();
+            }         
+
+        } catch (Exception e) {
+            System.out.println("Fail to read the file");
+        }
+        
+        int count = 1;
+        for (String keyword: keySet) {
+            System.out.println(count++ + "\t" + keyword);
+        }
 
         /*        
         String path = "C:\\Github\\ontologies\\config";
