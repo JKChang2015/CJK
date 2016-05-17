69a33df2f3e7e114c5146072f00cbe8416269a42
JKChang2015 <jkchang2015@gmail.com>
2016-05-17 16:08:43 +0100

    check label

--- CJK/src/cjk/CJK.java
+++ CJK/src/cjk/CJK.java
@@ -11,8 +11,36 @@ import java.io.StringReader;
 import java.io.File;
 import java.util.Set;
 import java.util.HashSet;
-import org.semanticweb.owlapi.*;
 
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
+import org.semanticweb.owlapi.model.AddAxiom;
+import org.semanticweb.owlapi.model.AddOntologyAnnotation;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLImportsDeclaration;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.model.RemoveImport;
+import org.semanticweb.owlapi.model.SetOntologyID;
+import org.semanticweb.owlapi.search.EntitySearcher;
+import org.semanticweb.owlapi.search.Searcher;
+import org.semanticweb.owlapi.util.OWLEntityRemover;
+import org.semanticweb.owlapi.util.OWLOntologyMerger;
+import org.semanticweb.owlapi.util.SimpleIRIMapper;
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 
 public class CJK {
 
@@ -21,6 +49,7 @@ public class CJK {
         String kPath = "C:\\Users\\jkchang\\Desktop\\keyword.txt";
         String ontoPath = "C:\\Users\\jkchang\\Desktop\\enanomapper.owl";
         Set<String> keySet = new HashSet<String>();
+        Set<OWLClass> entities = new HashSet<OWLClass>();
         Set<String> labelSet = new HashSet<String>();
 
         File file = new File(kPath);
@@ -30,15 +59,23 @@ public class CJK {
             while (line != null) {
                 keySet.add(line.trim());
                 line = reader.readLine();
-            }         
+            }
 
         } catch (Exception e) {
             System.out.println("Fail to read the file");
         }
-        
+
         int count = 1;
-        for (String keyword: keySet) {
+        for (String keyword : keySet) {
             System.out.println(count++ + "\t" + keyword);
+        }
+
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        try {
+            File ontoFile = new File(ontoPath);
+            OWLOntology onto = man.loadOntologyFromOntologyDocument(ontoFile);
+            entities = onto.getClassesInSignature();
+        } catch (Exception e) {
         }
 
         /*        
