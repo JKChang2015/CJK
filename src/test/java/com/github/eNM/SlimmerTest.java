4e0df9f40f89d3f39c8032335eeee5454826682d
JKChang2015 <jkchang2015@gmail.com>
2016-06-02 16:40:14 +0100

    updated

---
+++ src/test/java/com/github/eNM/SlimmerTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.eNM;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jkchang
+ */
+public class SlimmerTest {
+    
+    public SlimmerTest() {
+    }
+
+    @Test
+    public void testGetOntology() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+
+    @Test
+    public void testSaveAs_File_String() throws Exception {
+    }
+
+    @Test
+    public void testSaveAs_OutputStream_String() throws Exception {
+    }
+
+    @Test
+    public void testRemoveAllExcept() {
+    }
+
+    @Test
+    public void testRemoveAll() {
+    }
+    
+}
