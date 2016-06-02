b74ff6f4dbd6bd09c52aa82bdfb1cbc5141640dc
JKChang2015 <jkchang2015@gmail.com>
2016-06-02 15:05:31 +0100

    combine with old one

---
+++ src/main/java/com/github/eNM/IrisChecker.java
@@ -0,0 +1,89 @@
+package com.github.eNM;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * @Description Check syntax of iris file
+ * @author jkchang
+ */
+public class IrisChecker {
+
+    public void IrisChecker(File file) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine();
+        int lineNum = 1;
+
+        while (line != null) {
+            String instruction = line.trim();
+            if (instruction.length() == 0) {
+                System.out.println(file.getName() + ",  skip the empty line at " + lineNum+ "\n");
+                line = reader.readLine();
+                lineNum++;
+                continue;
+            }
+
+            char first = instruction.charAt(0);
+            char second = instruction.charAt(1);
+            char third = instruction.charAt(2);
+
+            if (first == '+') {
+                if (!containBracket(instruction)) {
+                    //  reader.close();
+                    System.out.println(file.getName() + ",  Invalid configuration input at line " + lineNum + " should have a SuperClass");
+                    System.out.println(instruction + "\n");
+                    line = reader.readLine();
+                    lineNum++;
+                    continue;
+                    // throw new Exception("Invalid configuration input at line " + lineNum + "should have a SuperClass");
+                }
+
+                int indexColon = instruction.indexOf(')') + 1;
+                if (instruction.charAt(indexColon) != ':') {
+                    //    reader.close();
+                    System.out.println(file.getName() + ",  Invalid configuration input at line " + lineNum + " should have a SubClass");
+                    System.out.println(instruction+ "\n");
+                    line = reader.readLine();
+                    lineNum++;
+                    continue;
+                    // throw new Exception("Invalid configuration input at line " + lineNum + "should have a SubClass");
+                }
+
+            } else if (first == '-') {
+                if (containBracket(instruction)) {
+                    //    reader.close();
+                    System.out.println(file.getName() + ",  Invalid configuration input at line " + lineNum + " should not have a SuperClass");
+                    System.out.println(instruction+ "\n");
+                    line = reader.readLine();
+                    lineNum++;
+                    continue;
+                    //  throw new Exception("Invalid configuration input at line " + lineNum + "should not have a SuperClass");
+                }
+            } else {  // first char should be +/-
+                //    reader.close();
+                System.out.println(file.getName() + ",  Invalid configuration input at line " + lineNum + " first character should be '+'  or '-'");
+                System.out.println(instruction+ "\n");
+                line = reader.readLine();
+                lineNum++;
+                continue;
+                // throw new Exception("Invalid configuration input at line " + lineNum + "first character should be '+'  or '-'");
+            }
+
+            line = reader.readLine();
+            lineNum++;
+
+        }
+        reader.close();
+
+    }
+
+    public boolean containBracket(String s) {
+        if (s.contains("(") && s.contains(")")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
