f3dd268e41bf22d46517c360b9d25bfc660dd746
JKChang2015 <jkchang2015@gmail.com>
2016-06-09 16:09:51 +0100

    add something

---
+++ src/main/java/com/github/cjk/exl.java
@@ -0,0 +1,27 @@
+package com.github.cjk;
+
+import java.io.File;
+import jxl.Workbook;
+import java.lang.Exception;
+import jxl.write.WritableWorkbook;
+import jxl.write.WritableSheet;
+import jxl.write.Label;
+
+public class exl {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\Users\\jkchang\\Desktop\\result.xls");
+        WritableWorkbook myexcel = Workbook.createWorkbook(file);
+        WritableSheet sheet1 = myexcel.createSheet("new sheet", 0);
+        Label l1 = new Label(0, 0, "first");
+        Label l2 = new Label(0, 1, "second");
+        
+        sheet1.addCell(l1);
+        sheet1.addCell(l2);
+        myexcel.write();
+        myexcel.close();
+        
+
+    }
+
+}
