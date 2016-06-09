c59833cc236280f663303af59e257ccd14d1b202
JKChang2015 <jkchang2015@gmail.com>
2016-06-09 13:29:11 +0100

    repaire

--- src/main/java/com/github/cjk/CJK.java
+++ src/main/java/com/github/cjk/CJK.java
@@ -1,52 +1,11 @@
 package com.github.cjk;
 
 import com.github.eNM.Label;
+import com.github.eNM.KeywordFile;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.HashSet;
 import java.util.Set;
-
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.AddAxiom;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.search.Searcher;
-import org.semanticweb.owlapi.model.OWLLiteral;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.RemoveImport;
-import org.semanticweb.owlapi.model.OWLAnnotation;
-import org.semanticweb.owlapi.model.SetOntologyID;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.util.SimpleIRIMapper;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLDataProperty;
-import org.semanticweb.owlapi.search.EntitySearcher;
-import org.semanticweb.owlapi.util.OWLEntityRemover;
-import org.semanticweb.owlapi.util.OWLOntologyMerger;
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLAnnotationValue;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.AddOntologyAnnotation;
-import org.semanticweb.owlapi.model.OWLAnnotationProperty;
-import org.semanticweb.owlapi.model.OWLImportsDeclaration;
-import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
-import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
-import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLOntologyStorageException;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLLiteral;
-import uk.ac.manchester.cs.owl.owlapi.*;
+import java.util.HashSet;
+import java.io.File;
 
 /**
  *
@@ -60,112 +19,18 @@ public class CJK {
         String ontoPath = "src\\main\\resources\\enm.owl";
         String ontoTest = "src\\main\\resources\\fruit.owl";
 
+        File ontoFile = new File(ontoPath);
+        File keywordFile = new File(kPath);
+
         Set<String> keywords = new HashSet<String>();
-        Set<String> ontoLabel = new HashSet<String>();
+        Set<String> ontoLabels = new HashSet<String>();
 
-        // 1. Load keywords
-        try {
-            BufferedReader keywordReader = new BufferedReader(new FileReader(kPath));
-            String line = keywordReader.readLine().trim();
-            while (line != null) {
-                line = keywordReader.readLine().trim();
-                keywords.add(line);
-            }
-            System.out.println("\n");
-        } catch (Exception e) {
-            System.out.println("Fail to load the keyword file form " + kPath);
-        }
+        KeywordFile kf = new KeywordFile(keywordFile);
+        keywords = kf.getkeywords();
+        Label lb = new Label(ontoFile);
+        ontoLabels = lb.getlabel();
 
-        // record the results
-        PrintWriter out = new PrintWriter(new FileWriter("labels.txt", true), true);
-        int count = 1;
+        System.out.println("finished");
 
-        //2. Load ontology labels
-        try {
-            File file = new File(ontoPath);
-            OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-            OWLDataFactory factory = man.getOWLDataFactory();
-            OWLOntology onto = man.loadOntologyFromOntologyDocument(file);
-            Set<OWLClass> classes = onto.getClassesInSignature();
-
-            for (OWLClass en : classes) {
-                System.out.println(count + ".  " + en.toString() + "\t\t\t\t ");
-                //out.println(count + ".  " + en.toString() + "\t\t\t\t ");
-                count++;
-                Set<OWLAnnotationAssertionAxiom> ann = onto.getAnnotationAssertionAxioms(en.getIRI());
-                for (OWLAnnotationAssertionAxiom axiom : ann) {
-                    if (axiom.getProperty().equals(factory.getRDFSLabel()) && axiom.getValue() instanceof OWLLiteral) {
-                        OWLLiteral lr = (OWLLiteral) axiom.getValue();
-                        String result = (String) lr.getLiteral();
-                        System.out.println(result + "\n");
-                        out.println(result);
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Fail to load the OWLfile form " + ontoPath);
-        }
-        out.close();
-
-        /*  
-        
-        String kPath = "C:\\Users\\jkchang\\Desktop\\keyword.txt";
-        String ontoPath = "C:\\Users\\jkchang\\Desktop\\enanomapper.owl";
-        String ontoTest = "C:\\Users\\jkchang\\Desktop\\fruit.owl";
-        Set<String> keySet = new HashSet<String>();
-        Set<OWLClass> entities = new HashSet<OWLClass>();
-        Set<String> labelSet = new HashSet<String>();
-
-        File file = new File(kPath);
-
-        try {
-            BufferedReader keywordReader = new BufferedReader(new FileReader(file));
-            String line = keywordReader.readLine();
-            while (line != null) {
-                keySet.add(line.trim());
-                line = keywordReader.readLine();
-            }
-
-        } catch (Exception e) {
-            System.out.println("Fail to read the file");
-        }
-
-        int count = 1;
-        for (String keyword : keySet) {
-            System.out.println(count++ + "\t" + keyword);
-        }
-
-        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-
-        try {
-            File ontoFile = new File(ontoTest);
-            OWLOntology onto = man.loadOntologyFromOntologyDocument(ontoFile);
-            Set<OWLClass> cls = onto.getClassesInSignature();
-            for (OWLClass cl : cls) {
-                String name = cl.toString();
-                System.out.println(name);
-            }
-        } catch (Exception e) {
-            System.out.println("fail to load the file");
-        }
-
-        System.out.println("lll");
-
-             
-        String path = "C:\\Github\\ontologies\\config";
-        File folder = new File(path);
-        File[] files = folder.listFiles();
-
-        for (File file : files) {
-            if (file.toString().endsWith("iris")) {
-                try {
-                    IrisChecker checker = new IrisChecker();
-                    checker.IrisChecker(file);
-                } catch (Exception e) {
-                    System.out.println("can't load the file");
-                }
-            }
-        }
-         */
     }
 }
--- src/main/java/com/github/eNM/Label.java
+++ src/main/java/com/github/eNM/Label.java
@@ -1,18 +1,22 @@
 package com.github.eNM;
 
 import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Set;
 import java.util.HashSet;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.search.EntitySearcher;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLLiteral;
 
 public class Label {
 
@@ -20,24 +24,29 @@ public class Label {
     private OWLOntology onto;
     private Set<String> labels = new HashSet<String>();
 
-    public Label(String owlPath) throws OWLOntologyCreationException {
+    public Label(File file) throws OWLOntologyCreationException, FileNotFoundException, IOException {
+//        PrintWriter out = new PrintWriter(new FileWriter("labels.txt", true), true);
+        int count = 0;
 
-        InputStream stream = this.getClass().getClassLoader().getResourceAsStream(owlPath);
-        man = OWLManager.createOWLOntologyManager();
-        OWLDataFactory fatory = man.getOWLDataFactory();
-        onto = man.loadOntologyFromOntologyDocument(stream);
-        
-        Set<OWLClass> entities = onto.getClassesInSignature();
-        
-        System.out.println("");
-        System.out.println("");
+        man = OWLManager.createOWLOntologyManager(); // Creat ontology manager
+        OWLDataFactory factory = man.getOWLDataFactory(); // Creat ontology factory
+        onto = man.loadOntologyFromOntologyDocument(file); // load ontology from file
+        Set<OWLClass> classes = onto.getClassesInSignature(); // load all the classes in Signature
 
-        for (OWLClass clas : entities) {
-            for (OWLAnnotation annot : EntitySearcher.getAnnotations(clas, onto)) {
-                String lab = annot.getValue().toString();
+        for (OWLClass clazz : classes) {
+            count++;
+            Set<OWLAnnotationAssertionAxiom> annotations = onto.getAnnotationAssertionAxioms(clazz.getIRI());  //get all the Annotation Assertion of 
+            for (OWLAnnotationAssertionAxiom annotation : annotations) {
+                if (annotation.getProperty().equals(factory.getRDFSLabel()) && annotation.getValue() instanceof OWLLiteral) {
+                    OWLLiteral lr = (OWLLiteral) annotation.getValue();
+                    String result = (String) lr.getLiteral();
+                    System.out.println(count + ".  "+result);
+//                    out.println(result);
+                }
             }
         }
 
+  //      out.close();
     }
 
     public Set<String> getlabel() {
