602a1cd1c5a2b8d1dba370f0ca49d7f6f92daec8
jkchang2015 <jkchang2015@gmail.com>
2016-06-21 14:39:11 +0100

    wiggle sort

--- LeetCode/src/main/java/leetcode/wiggleSort.java
+++ LeetCode/src/main/java/leetcode/wiggleSort.java
@@ -13,8 +13,9 @@ package leetcode;
 import java.util.Arrays;
 
 public class wiggleSort {
-
-    public void wiggleSort(int[] nums) {  //1. Sorted 2. exchange every two ele
+    
+    public void wiggleSort(int[] nums) {  //O(NlogN) O(1)
+        //1. Sorted 2. exchange every two elements
         Arrays.sort(nums);
         for (int i = 1; i < nums.length - 1; i += 2) {
             int temp = nums[i];
@@ -22,9 +23,17 @@ public class wiggleSort {
             nums[i + 1] = temp;
         }
     }
-
-    public void wiggleSort2(int[] nums) {
-
+    
+    public void wiggleSort2(int[] nums) {  //O(N) O(1)
+        // A1<A2>A3<A4>A5
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (((i + 1) % 2 == 1 && nums[i] < nums[i + 1]) //odd
+                    || ((i + 1) % 2 == 0 && nums[i] > nums[i + 1])) { //even
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+        }
     }
-
+    
 }
--- LeetCode/src/test/java/leetcode/wiggleSortTest.java
+++ LeetCode/src/test/java/leetcode/wiggleSortTest.java
@@ -14,20 +14,39 @@ import leetcode.wiggleSort;
  * @author jkchang
  */
 public class wiggleSortTest {
-
+    
     public wiggleSortTest() {
     }
-
+    
     @Test
-    public void testSomeMethod() {
+    public void WiggleSort() {
         int[] nums = {1, 3, 4, 2, 6, 5, 8};
         wiggleSort ws = new wiggleSort();
         ws.wiggleSort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.append(nums[i] + " ");
+        printfr(nums);
+    }
+    
+    @Test
+    public void WiggleSort2() {
+        int[] nums = {1, 3, 4, 2, 6, 5, 8, 10, 21, 4};
+        wiggleSort ws = new wiggleSort();
+        ws.wiggleSort2(nums);
+        printfr(nums);        
+    }
+    
+    public void printfr(int[] nums) {
+        System.out.append(Integer.toString(nums[0]));
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] > nums[i]) {
+                System.out.append(" > ");
+            } 
+            
+            else {
+                System.out.append(" < ");
+            }
+            System.out.append(Integer.toString(nums[i]));
         }
         System.out.println("");
-
     }
-
+    
 }
