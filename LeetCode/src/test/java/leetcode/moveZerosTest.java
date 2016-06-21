77bf95e4ac68409d8ceae21a474c45ca793ada81
jkchang2015 <jkchang2015@gmail.com>
2016-06-21 16:47:38 +0100

    add test

---
+++ LeetCode/src/test/java/leetcode/moveZerosTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import leetcode.moveZeros;
+/**
+ *
+ * @author jkchang
+ */
+public class moveZerosTest {
+    
+    public moveZerosTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        int[] nums = { 0, 1, 0, 3, 0, 12, 4, 0};
+        moveZeros mz = new moveZeros();
+        mz.moveZeroes(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.append(nums[i] + "  ");
+        }
+                System.out.println("");
+    }
+    
+}
