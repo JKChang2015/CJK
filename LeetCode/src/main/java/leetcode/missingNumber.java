2926864eb44c27ba2fc4df97e6166c1fdb7f7461
jkchang2015 <jkchang2015@gmail.com>
2016-06-23 16:38:11 +0100

    add

--- LeetCode/src/main/java/leetcode/missingNumber.java
+++ LeetCode/src/main/java/leetcode/missingNumber.java
@@ -1,13 +1,49 @@
 /* ----------------------------------------------------------------------------
+268. Missing Number 
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find 
+the one that is missing from the array.
 
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it 
+using only constant extra space complexity?
    ---------------------------------------------------------------------------- 
  @author jkchang
  @date 23-Jun-2016
-*/
-
+ */
 package leetcode;
 
-
 public class missingNumber {
+
+    public int missingNumber(int[] nums) {
+        // res = non-missing sum - missing sum
+        int len = nums.length;
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+        }
+        int res = (len + 1) * len / 2 - sum;
+        return res;
+    }
+
+    public int missingNumber2(int[] nums) {
+        //binary searching
+
+        return 0;
+    }
+
+    public int missingNumber3(int[] nums) {
+        // XOR
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            x ^= i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            x ^= nums[i];
+        }
+        return x;
+    }
 
 }
---
+++ LeetCode/src/test/java/leetcode/findDuplicateTest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import leetcode.findDuplicate;
+
+/*
+ * @author jkchang
+ * @date 23-Jun-2016
+ */
+public class findDuplicateTest {
+
+    public findDuplicateTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 4, 7, 8, 9};
+        findDuplicate fd = new findDuplicate();
+        int res = fd.findDuplicate5(arr);
+        System.out.println("Duplicate number is  " + res);
+    }
+
+}
