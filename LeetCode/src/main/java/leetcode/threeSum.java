3213dbc77f495218ff6e5d7b230dd26ecc5674f1
jkchang2015 <jkchang2015@gmail.com>
2016-06-24 15:45:37 +0100

    add something

---
+++ LeetCode/src/main/java/leetcode/threeSum.java
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------------
+15. 3Sum 
+Total Accepted: 124963 Total Submissions: 650797 Difficulty: Medium
+Given an array S of n integers, are there elements a, b, c in S such that 
+a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+   ---------------------------------------------------------------------------- 
+ @author jkchang
+ @date 24-Jun-2016
+ */
+package leetcode;
+
+import java.util.List;
+
+public class threeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
+}
---
+++ LeetCode/src/main/java/leetcode/threeSumSmaller.java
@@ -0,0 +1,26 @@
+/* ----------------------------------------------------------------------------
+3Sum Smaller
+Given an array of n integers nums and a target, find the number of index 
+triplets i, j, k with 0 <= i < j < k < n that satisfy the condition
+nums[i] + nums[j] + nums[k] < target.
+
+For example, given nums = [-2, 0, 1, 3], and target = 2.
+
+Return 2. Because there are two triplets which sums are less than 2:
+
+[-2, 0, 1]
+[-2, 0, 3]
+Follow up: Could you solve it in O(n2) runtime?
+   ---------------------------------------------------------------------------- 
+ @author jkchang
+ @date 24-Jun-2016
+ */
+package leetcode;
+
+public class threeSumSmaller {
+
+    public int threeSumSmaller(int[] nums, int target) {
+
+        return 0;
+    }
+}
