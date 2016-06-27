dd5f81c4d3692d540acd6858d03f64e88db9679f
jkchang2015 <jkchang2015@gmail.com>
2016-06-27 19:32:01 +0100

    list

--- LeetCode/src/test/java/leetcode/missingNumberTest.java
+++ LeetCode/src/test/java/leetcode/missingNumberTest.java
@@ -14,11 +14,11 @@ public class missingNumberTest {
 
     @Test
     public void testSomeMethod() {
-        int[] arr = { 0, 1, 2, 3,5};
+        int[] arr = {0, 1, 2, 3, 5};
         missingNumber nm = new missingNumber();
         int res = nm.missingNumber(arr);
         System.out.println("resul is " + res);
-        
+
     }
 
 }
