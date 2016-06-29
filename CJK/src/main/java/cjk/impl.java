2fc84cceeeb7eaebb124faedd11b66194baac1f1
jkchang2015 <jkchang2015@gmail.com>
2016-06-29 14:54:36 +0100

    u

--- CJK/src/main/java/cjk/impl.java
+++ CJK/src/main/java/cjk/impl.java
@@ -3,8 +3,7 @@
    ---------------------------------------------------------------------------- 
  @author jkchang
  @date 29-Jun-2016
-*/
-
+ */
 package cjk;
 
 import java.io.File;
@@ -12,13 +11,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
-
-
 public class impl {
 
     public static void main(String[] args) {
-        String path = "C:";
-        
-        
+        String folerPath = "C:\\Github\\ontologies\\external\\";
+        File file = new File(folerPath);
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                if (f.toString().endsWith("owl")) {
+                    String name = f.getName();
+                    System.out.println(name);
+                }
+
+            }
+
+        }
+
     }
 }
