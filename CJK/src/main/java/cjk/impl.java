af7a05c16ef819f65023f4f4555339ce2f7b6e40
jkchang2015 <jkchang2015@gmail.com>
2016-06-29 14:20:14 +0100

    modified

---
+++ CJK/src/main/java/cjk/impl.java
@@ -0,0 +1,24 @@
+/* ----------------------------------------------------------------------------
+
+   ---------------------------------------------------------------------------- 
+ @author jkchang
+ @date 29-Jun-2016
+*/
+
+package cjk;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
+
+public class impl {
+
+    public static void main(String[] args) {
+        String path = "C:";
+        
+        
+    }
+}
