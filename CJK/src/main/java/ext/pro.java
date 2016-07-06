3a6eec040495d8a99275dafcc812c65ecf719f0e
jkchang2015 <jkchang2015@gmail.com>
2016-07-06 16:12:22 +0100

    properties

---
+++ CJK/src/main/java/ext/pro.java
@@ -0,0 +1,42 @@
+/* ----------------------------------------------------------------------------
+java.util.properties
+   ---------------------------------------------------------------------------- 
+ @author jkchang
+ @date 06-Jul-2016
+ */
+package ext;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class pro {
+
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+        OutputStream output = null;
+
+        try {
+            output = new FileOutputStream("config.properties");
+            prop.setProperty("pro1", "ppp1");
+            prop.setProperty("pro2", "ppp2");
+            prop.setProperty("pro3", "ppp3");
+
+            prop.store(output, null);
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
