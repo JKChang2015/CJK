541321e276b5023284da900d7816d905ee03b684
jkchang2015 <jkchang2015@gmail.com>
2016-07-06 16:46:25 +0100

    load class properties

--- CJK/src/main/java/ext/proLoadPath.java
+++ CJK/src/main/java/ext/proLoadPath.java
@@ -3,11 +3,46 @@
    ---------------------------------------------------------------------------- 
  @author jkchang
  @date 06-Jul-2016
-*/
-
+ */
 package ext;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class proLoadPath {
 
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            String filename = "config.properties";
+            input = proLoadPath.class.getClassLoader().getResourceAsStream(filename);
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + filename);
+                return;
+            }
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            //get the property value and print it out
+            System.out.println(prop.getProperty("database"));
+            System.out.println(prop.getProperty("dbuser"));
+            System.out.println(prop.getProperty("dbpassword"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
