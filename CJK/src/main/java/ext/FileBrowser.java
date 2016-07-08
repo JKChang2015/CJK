a7d8b8a6f6f9aa1d11e93478289f127bc63dc953
jkchang2015 <jkchang2015@gmail.com>
2016-07-08 11:27:57 +0100

    update

--- CJK/src/main/java/ext/FileBrowser.java
+++ CJK/src/main/java/ext/FileBrowser.java
@@ -13,10 +13,6 @@ public class FileBrowser {
 
     String proPath = "C:\\Github\\ontologies\\config\\";
     File folder = new File(proPath);
-    FilenameFilter filter = new FilenameFilter() {
-        public boolean accept(File folder, String name) {
-            return name.toLowerCase().endsWith(".props");
-        }
-    };
+    FilenameFilter filter = (File folder1, String name) -> name.toLowerCase().endsWith(".props");
 
 }
