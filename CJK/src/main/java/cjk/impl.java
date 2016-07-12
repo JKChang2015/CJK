bb952bb6b4f6f11e8dc654323c1c8ce9aa9b8499
jkchang2015 <jkchang2015@gmail.com>
2016-07-12 16:29:14 +0100

    LSlimmer

--- CJK/src/main/java/cjk/impl.java
+++ CJK/src/main/java/cjk/impl.java
@@ -13,26 +13,43 @@ import javax.imageio.ImageIO;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.Set;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLImportsDeclaration;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 public class impl {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        File file = new File("..//ontologies//bao_complete.owl");
+        OWLOntology onto = man.loadOntologyFromOntologyDocument(file);
+        Set<OWLImportsDeclaration> importDeclarations = onto.getImportsDeclarations();
         
-        System.out.println(System.getenv("WORKSPACE"));
         System.out.println("");
-
-        String folerPath = "C:\\Github\\ontologies\\external\\";
-        File file = new File(folerPath);
-        if (file.exists()) {
-            File[] files = file.listFiles();
-            for (File f : files) {
-                if (f.toString().endsWith("owl")) {
-                    String name = f.getName();
-                    System.out.println(name);
-                }
-
-            }
+        for (OWLImportsDeclaration declaration : importDeclarations) {
 
         }
+
+        System.out.println("");
+
+//        System.out.println(System.getenv("WORKSPACE"));
+//        System.out.println("");
+//
+//        String folerPath = "C:\\Github\\ontologies\\external\\";
+//        File file = new File(folerPath);
+//        if (file.exists()) {
+//            File[] files = file.listFiles();
+//            for (File f : files) {
+//                if (f.toString().endsWith("owl")) {
+//                    String name = f.getName();
+//                    System.out.println(name);
+//                }
+//
+//            }
+//
+//        }
     }
 }
