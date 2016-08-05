9b136c56e4b776621e9613248451e7deeb59b115
jkchang2015 <jkchang2015@gmail.com>
2016-08-05 14:45:36 +0100

    mapper

--- CJK/src/main/java/eNM/Mapper.java
+++ CJK/src/main/java/eNM/Mapper.java
@@ -11,8 +11,13 @@ import java.util.Map;
 import java.util.HashSet;
 import java.util.Set;
 import eNM.MapTerm;
+import java.io.InputStream;
+import java.io.File;
+import java.io.FilenameFilter;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 public class Mapper {
@@ -22,7 +27,25 @@ public class Mapper {
     private OWLOntology onto;
 
     public static void main(String[] args) {
+        String rootFolder = "..\\ontologies\\config";
+        System.out.println("Loading configuration files in folder " + rootFolder);
 
+        File dir = new File(rootFolder);
+        File[] files = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".props");
+            }
+        });
+
+        for (File file : files) {
+
+        }
+
+    }
+
+    void combine(InputStream owlFile, String mergedOntologyIRI) throws OWLOntologyCreationException {
+        man = OWLManager.createConcurrentOWLOntologyManager();
+        onto = man.loadOntologyFromOntologyDocument(owlFile);
     }
 
 }
