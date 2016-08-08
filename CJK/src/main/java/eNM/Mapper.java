13e58e09c86e909ceadb48e76977f91e01e645ea
jkchang2015 <jkchang2015@gmail.com>
2016-08-08 11:24:49 +0100

    mapper

--- CJK/src/main/java/eNM/Mapper.java
+++ CJK/src/main/java/eNM/Mapper.java
@@ -1,9 +1,3 @@
-/* ----------------------------------------------------------------------------
-
-   ---------------------------------------------------------------------------- 
- @author jkchang
- @date 04-Aug-2016
- */
 package eNM;
 
 import java.util.HashMap;
@@ -14,6 +8,8 @@ import eNM.MapTerm;
 import java.io.InputStream;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.AddOntologyAnnotation;
 import org.semanticweb.owlapi.model.IRI;
@@ -25,21 +21,32 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.util.OWLOntologyMerger;
 
+/* 
+ @author jkchang
+ @date 04-Aug-2016
+ Task: mapping query terms with the external ontologies
+Steps: 1. load query terms
+       2. laad ontologies
+        2.1 merging ontologies
+        2.2 normalization
+       3. mapping terms with each ontologies
+       
+ */
 public class Mapper {
 
     private Map<String, MapTerm> mapping;
     private OWLOntologyManager man;
     private OWLOntology onto;
-    private HashSet<String> kWords;
+    private HashSet<String> keySet;
 
     public static void main(String[] args) {
+
+        //keywords
+        String keywordPath = "";
+
+        //ontology documents
         String rootFolder = "..\\ontologies\\config";
         System.out.println("Loading configuration files in folder " + rootFolder);
-        
-        //keywords
-        
-        
-        //ontology documents
         File dir = new File(rootFolder);  //filted the folder
         File[] files = dir.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
@@ -50,12 +57,23 @@ public class Mapper {
         for (File file : files) {
             
             // 1. combine the ontology
-
         }
 
     }
-    
-    
+
+    public void loadKeyword(String path) {
+        File kFile = new File(path);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(kFile));
+            String line = reader.readLine();
+            while (line != null) {
+                keySet.add(line.trim());
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("Fail to read the keyword file");
+        }
+    }
 
     public void combine(InputStream owlFile, String mergedOntologyIRI) throws OWLOntologyCreationException {
         man = OWLManager.createConcurrentOWLOntologyManager();
