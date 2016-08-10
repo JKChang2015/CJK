ae90ab793ca1f0e76b98bfaf10431a5be6a13def
jkchang2015 <jkchang2015@gmail.com>
2016-08-10 12:21:24 +0100

    update

--- CJK/src/main/java/eNM/Mapper.java
+++ CJK/src/main/java/eNM/Mapper.java
@@ -3,10 +3,21 @@ package eNM;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.OutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -54,12 +65,34 @@ public class Mapper {
 
         String kPath = "src\\main\\resources\\keyword.txt";
         File keywordFile = new File(kPath);
-        
+        System.out.println("Loaded keywordFile from " + kPath);
+
+        String rootFolder = "..\\ontologies\\config";
+        System.out.println("Searching configuration files in folder " + rootFolder);
+        File dir = new File(rootFolder);
+        File[] files = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".props");
+            }
+        });
+
+        for (File file : files) {  // for each property file
+            try {
+
+            } catch (Exception e) {
+
+            }
+        }
+
         String ontoPath = " ";
         File ontoFile = new File(ontoPath);
-        
+
         Mapper map = new Mapper(keywordFile, ontoFile);
         HashSet<String> set = (HashSet) map.keywords;
+    }
+
+    public void combine() {
+
     }
 
 }
