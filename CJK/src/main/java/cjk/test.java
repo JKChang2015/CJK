7a86979f3a6de5e2d9edf87448b61af406761b61
jkchang2015 <jkchang2015@gmail.com>
2016-08-15 14:21:53 +0100

    update

--- CJK/src/main/java/cjk/test.java
+++ CJK/src/main/java/cjk/test.java
@@ -14,39 +14,62 @@ import java.util.HashSet;
 import java.util.Properties;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AddOntologyAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.util.OWLOntologyMerger;
 
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OWLOntologyCreationException {
 
         //test combine ontologies
+        String owlURL = "";
+        String owlFilename = "";
+
         String ontoPath = "..\\ontologies\\config\\bao.props";
         File file = new File(ontoPath);
-        OWLOntology comOnto;
-
         try {
             System.out.println("loading owl config file from " + ontoPath);
             Properties props = new Properties();
             props.load(new FileReader(file));
 
-            String owlURL = props.getProperty("owl");
-            String owlFilename = owlURL;
+            owlURL = props.getProperty("owl");
+            owlFilename = owlURL;
             if (owlFilename.contains("/")) {
                 owlFilename = owlFilename.substring(owlFilename.lastIndexOf('/') + 1);
             }
-
-            OWLOntologyManager man = OWLManager.createConcurrentOWLOntologyManager();
-            OWLOntology onto = man.loadOntology(IRI.create(owlURL));
-            System.out.println("");
-
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("fail to load the property file");
         }
 
-        //test loading terms from keyword file
+        OWLOntologyManager man = OWLManager.createConcurrentOWLOntologyManager();; //onto manager
+        OWLOntologyMerger merger = new OWLOntologyMerger(man);
+        OWLOntology onto = man.loadOntologyFromOntologyDocument(IRI.create(owlURL)); //input
+        System.out.println("size of the onto is " + onto.getClassesInSignature().size());
+        System.out.println("Loaded ontology:" + owlFilename);
+        onto = merger.createMergedOntology(man, IRI.create(owlURL + "_merg")); //output
+
+        for (OWLOntology ontology : man.getOntologies()) {
+            System.out.println(" Copying annotations from " + ontology.getOntologyID());
+
+            for (OWLAnnotation annotation : ontology.getAnnotations()) {
+                //System.out.println(" Copying annotation: " + annotation.getProperty() + " -> " + annotation.getValue());
+                AddOntologyAnnotation annotationAdd = new AddOntologyAnnotation(onto, annotation);
+                man.applyChange(annotationAdd);
+            }
+        }
+        System.out.println("size of the merged onto is " + onto.getClassesInSignature().size());
+        System.out.println("\n\n\n" + "======================");
+        System.out.println("merged: " + owlFilename);
+    }
+
+}
+
+//test loading terms from keyword file
 //        String kPath = "src\\main\\resources\\chemical description terms.txt";
 //        File file = new File(kPath);
 //        Set<String> keySet = new HashSet<String>();
@@ -58,6 +81,4 @@ public class test {
 //            e.printStackTrace();
 //            System.out.println("fail to load the keyword file");
 //        }
-    }
 
-}
