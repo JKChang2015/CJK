75fde8b1ec5b746aa3aaf03ad1d06bba566c728f
jkchang2015 <jkchang2015@gmail.com>
2016-09-05 21:06:47 +0100

    add test

---
+++ CJK/src/test/java/ext/FileBrowserTest.java
@@ -0,0 +1,25 @@
+package ext;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+ * @date Sep 5, 2016
+ * @author JKChang
+ */
+public class FileBrowserTest {
+
+    public FileBrowserTest() {
+    }
+
+    @Test
+    public void testListFile() {
+        String rootFolder = "..\\ontologies\\config\\";
+        String fileType = "iris";
+        
+        FileBrowser ft = new FileBrowser();
+        ft.listFile(rootFolder, fileType);
+        
+    }
+
+}
