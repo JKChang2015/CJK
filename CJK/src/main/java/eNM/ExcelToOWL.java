928a79d4970ed7d2bd628dea94b6a503eb02b80a
jkchang2015 <jkchang2015@gmail.com>
2016-10-14 16:36:59 +0100

    add excelToOWL.java

---
+++ CJK/src/main/java/eNM/ExcelToOWL.java
@@ -0,0 +1,26 @@
+package eNM;
+
+/**
+ * Load term list from excel file and add to OWL file. Function: 1. load excel
+ * (term name, superclass and definition) 2. load owlfile as txt file 3. convert
+ * term list to owl file 4. save the owl file
+ *
+ * The basic information need to be added to ontology: 1. term name 2. url 3.
+ * term label 4. definition 5. superclass
+ *
+ * If possible record the new term's url to output
+ *
+ * @author jkchang
+ */
+public class ExcelToOWL {
+
+    double newTermStartID = 0;
+    String TermURI = "";
+    String owlFile = " ";
+    String termFile = " ";
+
+    public static void main(String[] args) {
+
+    }
+
+}
