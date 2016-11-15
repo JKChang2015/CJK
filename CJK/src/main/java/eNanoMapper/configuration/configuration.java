650fa1e4a1fc72b68dfed98c007e97c6e98585ba
JKChang2015 <jkchang2015@gmail.com>
2016-11-15 16:06:14 +0000

    update

--- CJK/src/main/java/eNanoMapper/configuration/configuration.java
+++ CJK/src/main/java/eNanoMapper/configuration/configuration.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eNanoMapper.configuration;
 
 /**
  *
- * @author jkchang
+ * @author JKChang
+ * @date 15-Nov-2016
+ * @Description:
+ *
  */
+
 public class configuration {
-    
+
 }
