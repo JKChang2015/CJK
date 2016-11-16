7db1f2ac1e1b3aa7f2ba18649b2ce355f0ae4047
jkchang2015 <jkchang2015@gmail.com>
2016-11-16 14:39:28 +0000

    student

---
+++ CJK/src/main/java/cjk/Student.java
@@ -0,0 +1,59 @@
+package cjk;
+
+/**
+ *
+ * @author JKChang
+ * @date 16-Nov-2016
+ * @Description:
+ *
+ */
+
+public class Student {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
--- CJK/src/main/java/eNanoMapper/Util/FileBrowser.java
+++ CJK/src/main/java/eNanoMapper/Util/FileBrowser.java
@@ -1,4 +1,4 @@
-package util;
+package eNanoMapper.Util;
 
 import java.io.File;
 import java.io.FilenameFilter;
