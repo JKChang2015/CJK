f625c45f34c5c76a1f40489e9ad3ee02c94c1030
JKChang2015 <jkchang2015@gmail.com>
2016-11-22 14:01:48 +0000

    enum

---
+++ CJK/src/main/java/Demo/enum_Color.java
@@ -0,0 +1,15 @@
+package Demo;
+
+/**
+ *
+ * @author JKChang
+ * @date 22-Nov-2016
+ * @Description:
+ *
+ */
+
+public enum enum_Color {
+    
+    
+
+}
---
+++ CJK/src/main/java/Demo/enum_signal.java
@@ -0,0 +1,32 @@
+package Demo;
+
+/**
+ *
+ * @author JKChang
+ * @date 22-Nov-2016
+ * @Description: demonstration of 'enum' using in switch-case
+ *
+ */
+public class enum_signal {
+
+    public enum Signal {
+        GREEN, RED, YELLOW;
+    }
+
+    Signal color = Signal.RED;
+
+    public void change() {
+        switch (color) {
+            case RED:
+                color = Signal.GREEN;
+                break;
+            case YELLOW:
+                color = Signal.RED;
+                break;
+            case GREEN:
+                color = Signal.YELLOW;
+                break;
+        }
+    }
+
+}
