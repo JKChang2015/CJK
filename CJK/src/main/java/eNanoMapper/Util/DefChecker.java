8ce18a6a1080ef3f545876d8f1613f7e5512337c
JKChang2015 <jkchang2015@gmail.com>
2017-01-27 14:29:25 +0000

    defChecker

--- CJK/src/main/java/eNanoMapper/Util/DefChecker.java
+++ CJK/src/main/java/eNanoMapper/Util/DefChecker.java
@@ -19,13 +19,17 @@ import java.util.Set;
 import java.util.HashSet;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 
 public class DefChecker {
 
     //private OWLOntology onto;
-    private OWLOntologyManager man;
-
+    //private OWLOntologyManager man;
     public static void main(String[] args) throws OWLOntologyCreationException, FileNotFoundException {
 
         //load the list of the def URIs
@@ -37,7 +41,7 @@ public class DefChecker {
         while (s1.hasNext()) {
             URIs.add(s1.next());
         }
-        
+
         System.out.println("Total number of Definitions is " + URIs.size());
 
         //load the eNM ontology
@@ -48,8 +52,36 @@ public class DefChecker {
         OntoLoader loader = new OntoLoader(owlFile);
         loader.merge();
         OWLOntology onto = loader.getOnto();
+
+        // get Annotation Properties
+        int count = 0;
+//        OWLOntologyManager man = OWLManager.createConcurrentOWLOntologyManager();
+//        OWLDataFactory factory = man.getOWLDataFactory(); // Creat ontology factoryF
+        Set<OWLClass> classes = onto.getClassesInSignature(); // load all the classes in Signature
+        Map<String, String> defs = new HashMap<String, String>();
+
+        int unDef = 0;
+
+        for (OWLClass clazz : classes) { //for each classes
+            count++;
+            Set<OWLAnnotationAssertionAxiom> annotations = onto.getAnnotationAssertionAxioms(clazz.getIRI());  //get all the Annotation Assertion of 
+
+            boolean hasDef = false;
+            for (OWLAnnotationAssertionAxiom an : annotations) {
+                String anURI = an.getProperty().toStringID();
+                if (URIs.contains(anURI)) {
+                    hasDef = true;
+                    break;
+                }
+            }
+            if (!hasDef) {
+                unDef++;
+                System.out.println(clazz.getIRI().toString());
+            }
+
+        }
         
-        
+        System.out.println("Total number of un-defined term is " + unDef);
 
     }
 
