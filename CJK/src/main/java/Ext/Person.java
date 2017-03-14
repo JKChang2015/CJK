7fc6548830cf5dd13a08d89f8652bba05fc5e2ef
JKChang2015 <jkchang2015@gmail.com>
2017-03-14 12:31:56 +0000

    update

---
+++ CJK/src/main/java/Ext/Person.java
@@ -0,0 +1,31 @@
+package Ext;
+
+/**
+ *
+ * @author jkchang
+ * @date 14-Mar-2017
+ * @Description:
+ *
+ */
+public class Person {
+
+    public  String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
---
+++ CJK/src/main/java/Ext/Student.java
@@ -0,0 +1,19 @@
+package Ext;
+
+/**
+ *
+ * @author jkchang
+ * @date 14-Mar-2017
+ * @Description:
+ *
+ */
+public class Student extends Person {
+
+    int id;
+    String school;
+
+    public void setName() {
+        name = "David";
+    }
+
+}
