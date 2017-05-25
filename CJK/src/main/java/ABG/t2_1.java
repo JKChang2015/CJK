36e28d77bf88e07be4234a09ce4310d6419f5e51
JKChang2015 <jkchang2015@gmail.com>
2017-05-25 09:16:50 +0100

    r

---
+++ CJK/src/main/java/ABG/t2_1.java
@@ -0,0 +1,18 @@
+package ABG;
+
+/**
+ *
+ * @author jkchang
+ * @date 24-May-2017
+ * @Description:
+ *
+ */
+
+public class t2_1 {
+    public static void main(String[] args) {
+        int a = 1_2;
+        int b = 2_3;
+        System.out.println(a+b);
+        
+    }
+}
