fb590612c49b2f8269012923fe66d0b63aec3b84
JKChang2015 <jkchang2015@gmail.com>
2017-07-24 15:30:45 +0100

    test

--- java100/src/main/java/com/cjk/java100/j001.java
+++ java100/src/main/java/com/cjk/java100/j001.java
@@ -4,13 +4,25 @@ package com.cjk.java100;
  *
  * @author jkchang
  * @date 24-Jul-2017
- * @Description:
+ * @Description: print pyramid
  *
  */
-
 public class j001 {
-    public static void main(String[] args) {
-        System.out.println("print something");
-    }
 
+    public static void main(String[] args) {
+        int line = 4;
+
+        for (int i = 1; i <= line; i++) {
+
+            for (int j = 0; j < line - i; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print(" * ");
+            }
+
+            System.out.println(" ");
+        }
+    }
 }
