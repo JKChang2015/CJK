f5e3ddbbdbdea1e1543d82ffe8c952a3c315b30c
JKChang2015 <jkchang2015@gmail.com>
2018-09-02 22:29:49 +0100

    finish interface!

---
+++ CJK/src/main/java/Demo/test_demo7.java
@@ -0,0 +1,54 @@
+package Demo;
+
+/**
+ *
+ * @author jkchang
+ * @date 02-Sep-2018
+ * @Description:
+ *
+ */
+interface Subject {
+
+    public void make();
+}
+
+class RealSubject implements Subject {
+
+    @Override
+    public void make() {
+        System.out.println("making");
+    }
+}
+
+class ProxySubject implements Subject {
+
+    private Subject subject;
+
+    public ProxySubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void prepare() {
+        System.out.println("preparing for making...");
+    }
+
+    public void make() {
+        this.prepare();
+        this.subject.make();
+        this.finish();
+    }
+    
+    public void finish(){
+        System.out.println("Done...");
+    }
+}
+
+public class test_demo7 {
+
+    public static void main(String[] args) {
+        Subject sub = new ProxySubject(new RealSubject());
+        // 用户请求操作， 代理服务 帮助用户完成其余操作
+        sub.make();  //调用代理主题操作， 完成比真实更多的操作
+
+    }
+}
